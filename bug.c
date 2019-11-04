fd8b9bdc6c3a273fe609b279c61d213c04dd36a1
hyscript <danoyanhayk@gmail.com>
2019-11-04 16:26:25 +0400

    read and parse works: need code refactoring

--- bug.c
+++ bug.c
@@ -1,14 +1,105 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h>
+#include <jmorecfg.h>
+#include <string.h>
 
-char * read()
+size_t BUFSIZE = 1024; // be careful with this variable before changing its value
+
+int skipping_esccapes(char *line, int i_line)
 {
-    return 0;
+       while (line[i_line]==' ')
+        {
+            i_line++;
+        }
+    return i_line;
+}
+
+
+char *read()
+{
+    
+    char *buffer = malloc(BUFSIZE * sizeof(char));
+
+    if (!buffer)
+    {
+        fprintf(stderr,"Allocation error");
+        exit(1);
+    }
+
+    printf( "Enter a value :");
+    getline(&buffer,&BUFSIZE,stdin);
+
+    return buffer;
 }
 
 char **parse(char * line)
 {
+    //alocation of arguments array
+    char **args = (char **)malloc(BUFSIZE * sizeof(char *));
 
+    for (int i=0; i<BUFSIZE; i++)
+        args[i] = (char *)malloc(BUFSIZE * sizeof(char));
+    for (int j = 0; j < BUFSIZE; ++j)
+    {
+        for (int k = 0; k < BUFSIZE; ++k)
+        {
+            printf("%s", &args[j][k]);
+        }
+    }
+
+
+
+
+    int l_size =(int) sizeof(line)/sizeof(line[0]); // NOLINT(bugprone-sizeof-expression)
+    int i_line = 0;
+
+    //indecies of arguments
+    int i_args = 0;
+    int j_args = 0;
+    do
+    {
+        if ((int)line[i_line] == 32)
+        {
+            i_args++;
+            i_line = skipping_esccapes(line, i_line);
+            j_args=0;
+            args[i_args][j_args++] = line[i_line];
+
+
+        } else
+        {
+            args[i_args][j_args] = line[i_line];
+            j_args++;
+
+
+        }
+
+        char h = line[i_line];
+
+        i_line++;
+
+    }
+    while (line[i_line] != 0);
+//    printf("\n");
+//    int i=0;
+//    int j=0;
+//
+//    while ((int)args[i][j]!=10)
+//    {
+//        if((int)args[i][j]==0)
+//        {
+//           i++;
+//           j=0;
+//        } else
+//        {
+//            printf("%s", &args[i][j]);
+//            j++;
+//        }
+//
+//
+//    }
+//    printf("\n");
 }
 
 int execute(char ** args)
@@ -48,11 +139,12 @@ void run(void)
 
 int main(int argc, char** argv)
 {
-    //loading the configuration
+    /* loading the configuration
+     * run the program
+     * cleanup the program
+     * */
 
-    //run the program
-
-    //cleanup the program
+   parse( read());
 
     printf("Hello, World!\n");
     return 0;
