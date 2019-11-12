a65ff2df577a22d782795c31c0c6ba209f7ebfda
hyscript <danoyanhayk@gmail.com>
2019-11-12 22:03:03 +0400

    added new functionality cd - command path - ability to print correct path

--- bug_shell.c
+++ bug_shell.c
@@ -7,7 +7,101 @@
 
 size_t BUFSIZE = 1024; // be careful with this variable before changing its value
 int isInBackground = 0;
+char updated_path [2048];
 
+//counting the length of string
+int lenstr(const char * arr)
+{
+    int i = 0;
+    char stop = '\000';
+    while (1)
+    {
+        if (arr[i] == stop)
+            return i;
+        i++;
+    }
+
+    exit(0); //can't figure out why but after returning i program continue to execution
+    //this is temporary solution
+}
+
+//string concatination
+char * concatstr(const char *a, const char *b, int a_size, int b_size)
+{
+
+    int i_a = 0;
+
+    int c_size = a_size + b_size;
+
+    char * c =( char *)  malloc( c_size*sizeof(char));
+
+    for (i_a = 0; i_a < a_size; ++i_a)
+        c[i_a] = a[i_a];
+
+    for (int j = 0; j < b_size; ++j)
+        c[i_a++] = b[j];
+
+    return c;
+}
+
+void mergestr(char *to, char *from)
+{
+    int a_size = lenstr(to);
+    int b_size = lenstr(from);
+    /*int untill = 0;
+    int start = 0;
+
+    if(a_size >=b_size)
+    {
+        if(a_size != b_size)
+            start = b_size;
+        untill = a_size;
+    }
+    else
+    {
+        start = a_size;
+        untill = b_size;
+    }*/
+
+    for (int i = 0; i < b_size; ++i)
+        to[a_size++] = from[i];
+}
+
+
+int cmpstr(char * a, char *b)
+{
+    int a_size = lenstr(a);
+    int b_size = lenstr(b);
+
+    if (a_size != b_size)
+        return 0;
+    else
+    {
+        for (int i = 0; i < b_size; ++i)
+        {
+            if(a[i]!=b[i])
+                return 0;
+        }
+
+        return 1;
+    }
+}
+
+
+void cpstr(char *to, char *from)
+{
+    int to_size = lenstr(to);
+    int from_size = lenstr(from);
+
+    for (int i = 0; i < to_size; ++i)
+    {
+        if(i < from_size)
+            to[i]=from[i];
+        else
+            to[i] = 0;
+    }
+
+}
 
 const char *getUserName()
 {
@@ -22,33 +116,78 @@ const char *getUserName()
 }
 
 
+void update_path()
+{
+
+}
+
+
+void print_path(char * update_the_path)
+{
+    uid_t usr_type = getuid();
+    if(update_the_path[0]=='/')
+        cpstr(updated_path,update_the_path);
+    else
+    {
+        if(update_the_path[0]!='~')
+        {
+            int s_a = lenstr(updated_path);
+            int s_b = lenstr(update_the_path);
+            mergestr(updated_path, update_the_path);
+            updated_path[s_a + s_b] = '/';
+        }
+
+    }
+    if (usr_type == 0) {
+        printf("%s@bug_shell:%s# > ",getUserName(),updated_path);
+    }
+    else {
+        printf("%s@bug_shell:%s$ > ",getUserName(),updated_path);
+    }
+}
+
+
+
+
+void cd(char *path)
+{
+    chdir(path);
+
+    print_path(path);
+
+}
+
+
+
+
+
 
 void you_are_here(uid_t usr_type) {
 
     char cwd[2048];
     char home_pattern[] = {"/home/"}; //size = 7
-    char updated_path [2048];
-    //chdir("/path/to/change/directory/to");
+
+    int size_cwd = lenstr(cwd);
+
     getcwd(cwd, sizeof(cwd));
     int is_home_on_path = 1;
-    int username_length = 7; // to fix username count
+    int username_length = 7; // home pattern size
     for (int i = 0; i < 2048; ++i)
     {
         if (cwd[i]==0)
             break;
 
-        if (i < 6 && home_pattern[i] == cwd[i])
+        if (i < 6)
         {
-            username_length++;
-        }
+            if (home_pattern[i] == cwd[i])
+                username_length++;
+            else
+                is_home_on_path = 0;
 
-        else if(i < 6 && home_pattern[i] != cwd[i])
-        {
-            is_home_on_path = 0;
         }
     }
 
-    if (is_home_on_path == 1)
+    if (is_home_on_path == 1 && size_cwd > 1)
     {
         updated_path[0] = '~';
         for (int j = 0; j < 2048; ++j)
@@ -59,16 +198,10 @@ void you_are_here(uid_t usr_type) {
                 updated_path[j-username_length] = cwd[j];
         }
     }
-
-
-
-    if (usr_type == 0) {
-        printf("%s@bug_shell:%s# > ",getUserName(),updated_path);
-    }
-    else {
-        printf("%s@bug_shell:%s$ > ",getUserName(),updated_path);
-    }
+    print_path(updated_path);
 }
+
+
 
 
 
@@ -114,9 +247,10 @@ int execute(char * args[])
     }
     else if (pid == 0 && isInBackground == 1){
         //don' do anything cz this is child process
-    } else
-    {
+    }
 
+    else
+    {
 
         if (isInBackground == 0) {
             if (waitpid(pid, &status, 0) < 1)
@@ -153,43 +287,6 @@ int execute(char * args[])
     isInBackground = 0; //resetting background process
     return 1;
 
-}
-
-
-
-//counting the length of string
-int lenstr(const char * arr)
-{
-    int i = 0;
-    char stop = '\000';
-    while (1)
-    {
-        if (arr[i] == stop)
-            return i;
-        i++;
-    }
-
-    exit(0); //can't figure out why but after returning i program continue to execution
-             //this is temporary solution
-}
-
-//string concatination
-char * concatstr(const char *a, const char *b, int a_size, int b_size)
-{
-
-    int i_a = 0;
-
-    int c_size = a_size + b_size;
-
-    char * c =( char *)  malloc( c_size*sizeof(char));
-
-    for (i_a = 0; i_a < a_size; ++i_a)
-        c[i_a] = a[i_a];
-
-    for (int j = 0; j < b_size; ++j)
-        c[i_a++] = b[j];
-
-    return c;
 }
 
 void parse(char * line, char  * args[])
@@ -236,9 +333,10 @@ void run(void)
      * */
 
     int status = 1;
-    while (status > 0)
+    while (status >= 0)
     {
-        you_are_here(getuid());
+        if (status == 1)
+            you_are_here(getuid());
         char *line = malloc(BUFSIZE * sizeof(char));
 
         read_input(line);
@@ -247,19 +345,18 @@ void run(void)
         for (int j = 0; j < BUFSIZE; ++j)
             args[j] = 0;
 
-        int size = 0;
-
-        //for path determination, replacing home with ~
-        for (int i = 0; i < BUFSIZE; ++i)
-        {
-            if (line[i]==10) // 10 = /n
-                break;
-            size ++;
-        }
-
         parse(line,args);
         //set follow-fork-mode child set detach-on-fork off
-        status = execute(args);
+        if (cmpstr(args[0],"cd"))
+        {
+            cd(args[1]);
+            status = 0;
+        }
+
+        else
+        {
+            status = execute(args);
+        }
         free(line);//0x7fff33941160
 
     }
